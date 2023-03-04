faeec8074418878c8e2206267367f94879731efc
Mahdi Boomeri <mahdi@boomeri.dev>
2023-03-04 19:47:07 +0330

    feat: add todo

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -1,21 +1,32 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { Todo } from '../types/todo'
+import { useTodoStore } from '../stores/todo.store'
 import AppButton from './AppButton.vue'
 import AppInput from './AppInput.vue'
 
-defineProps<{
+const props = defineProps<{
   type: Todo['status']
 }>()
 
+const store = useTodoStore()
 const todo = ref('')
+
+function addTodo() {
+  store.add({
+    name: todo.value,
+    status: props.type,
+  })
+
+  todo.value = ''
+}
 </script>
 
 <template>
   <div class="flex gap-4">
     <AppInput v-model="todo" type="text" placeholder="Type your todo here" class="flex-grow" />
 
-    <AppButton intent="normal" :disabled="!todo">
+    <AppButton intent="normal" :disabled="!todo" @click="addTodo">
       Add Item
     </AppButton>
   </div>
