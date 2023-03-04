9775879100ec3af178fe939cac735a265b827d0d
Mahdi Boomeri <mahdi@boomeri.dev>
2023-03-04 19:46:24 +0330

    refactor: use map structure

--- src/stores/todo.store.ts
+++ src/stores/todo.store.ts
@@ -3,20 +3,37 @@ import { ref } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
 import type { Todo } from '../types/todo'
 
-export const useAlertsStore = defineStore('todo', () => {
-  const items = ref<Todo[]>([])
+export const useTodoStore = defineStore('todo', () => {
+  const items = ref<Map<string, Todo>>(new Map())
 
   function add(item: Omit<Todo, 'id' | 'createdAt'>) {
-    items.value.push({
-      id: uuidv4(),
+    items.value.set(uuidv4(), {
       createdAt: new Date(),
       name: item.name,
       status: item.status,
     })
   }
 
+  function remove(id: string) {
+    items.value.delete(id)
+  }
+
+  function setStatus(id: string, status: Todo['status']) {
+    const item = items.value.get(id)
+
+    if (!item)
+      return
+
+    items.value.set(id, {
+      ...item,
+      status,
+    })
+  }
+
   return {
     items,
     add,
+    remove,
+    setStatus,
   }
 })
--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -1,5 +1,4 @@
 export interface Todo {
-  id: string
   createdAt: Date
   name: string
   status: 'DONE' | 'PENDING' | 'BACKLOG'
